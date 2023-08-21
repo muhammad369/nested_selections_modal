8ccdcf2fbe7702124530f93099bd19fe6e98b62e
mohamed ali <mohamed@ali.com>
2023-08-21 13:00:40 +0300

    version 1.0.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nested_selection_dialog/nested_selection_dialog.dart';
+import 'package:nested_selection_dialog/nested_selection_modal.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,27 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,48 +31,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int? selection = null;
+  List<int> selections = []; // [2010,3,4];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           SelectionView(selctedIndex: 0, items: [0,1,2,3,4],)
+            ElevatedButton(
+                onPressed: () async {
+                  var r = await showNestedSelectionsModal<String>(context,
+                      initialSelections: [],
+                      items: _createItems()!,
+                      );
+                  print(r?.toString());
+                },
+                child: Text('show Nested Selection Modal')),
+            SizedBox(
+              height: 30,
+            ),
+            // NestedSelectionView<int>(
+            //     selections: selections,
+            //     onSelectionChanged: (s) {
+            //       selections = s;
+            //       setState(() {});
+            //     },
+            //     items: List.generate(30, (index) => index + 2000)
+            //         .map((e) => SelectionItemModel(
+            //             label: '$e',
+            //             value: e,
+            //             children: List.generate(
+            //                 12,
+            //                 (index) => SelectionItemModel(
+            //                     label: (index + 1).toString(),
+            //                     value: (index + 1),
+            //                     children: List.generate(30,
+            //                         (index) => SelectionItemModel(label: (index + 1).toString(), value: index + 1))))))
+            //         .toList())
           ],
         ),
       ),
-      );
+    );
+  }
+
+  List<SelectionItemModel<String>>? _createItems({String? parent = null, int level = 0}) {
+    if (level == 4) return null;
+    if (level == 0) {
+      return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+          .map((e) => SelectionItemModel(label: e, value: e, children: _createItems(parent: e, level: level + 1)))
+          .toList();
+    }
+    //
+    return List.generate(
+      10,
+      (index) => SelectionItemModel(
+          label: '$parent-${index + 1}',
+          value: '$parent-${index + 1}',
+          children: _createItems(parent: '$parent-${index + 1}', level: level + 1)),
+    );
   }
 }
---
+++ lib/nested_selection_modal.dart
@@ -0,0 +1,302 @@
+library nested_selection_modal;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Future<List<T>?> showNestedSelectionsModal<T>(BuildContext context,
+    {required List<SelectionItemModel<T>> items,
+    List<T>? initialSelections,
+    Widget Function(SelectionItemModel<T>)? itemBuilder,
+    Widget Function(SelectionItemModel<T>)? selectedItemBuilder,
+    double selectorHeight = 250,
+    double selectionMagnification = 1,
+    double itemHeight = 40,
+    double selectorWidth = 120,
+    String okText = 'Ok',
+    String cancelText = 'Cancel'}) {
+  return showModalBottomSheet<List<T>?>(
+    isDismissible: false,
+      constraints: BoxConstraints(maxHeight: selectorHeight + 150),
+      context: context,
+      builder: (context) {
+        List<T> selections = initialSelections ?? [];
+        return StatefulBuilder(
+            builder: (context, setState) => _NestedSelectionModal(
+                  items: items,
+                  okText: okText,
+                  cancelText: cancelText,
+                  selectorWidth: selectorWidth,
+                  selections: selections,
+                  selectorHeight: selectorHeight,
+                  selectionMagnification: selectionMagnification,
+                  itemHeight: itemHeight,
+                  itemBuilder: itemBuilder,
+                  selectedItemBuilder: selectedItemBuilder,
+                  onSelectionChanged: (s) {
+                    setState(() {
+                      selections = s;
+                    });
+                  },
+                ));
+      });
+}
+
+class _NestedSelectionModal<T> extends StatelessWidget {
+  final List<SelectionItemModel<T>> items;
+  final List<T>? selections;
+  final Widget Function(SelectionItemModel<T>)? itemBuilder;
+  final Widget Function(SelectionItemModel<T>)? selectedItemBuilder;
+  final double selectionMagnification;
+  final double selectorHeight;
+  final double itemHeight;
+  final double selectorWidth;
+  final void Function(List<T>) onSelectionChanged;
+  //
+  final String okText, cancelText;
+
+  const _NestedSelectionModal(
+      {super.key,
+      required this.items,
+      this.selections,
+      this.itemBuilder,
+      this.selectedItemBuilder,
+      this.selectorHeight = 250,
+      this.selectionMagnification = 1,
+      this.itemHeight = 40,
+      this.selectorWidth = 120,
+      required this.okText,
+      required this.cancelText,
+      required this.onSelectionChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  child: Text(okText),
+                  onPressed: () {
+                    Navigator.of(context).pop(selections);
+                  },
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  child: Text(cancelText),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+          NestedSelectionView<T>(
+            onSelectionChanged: onSelectionChanged,
+            items: items,
+            itemBuilder: itemBuilder,
+            selectedItemBuilder: selectedItemBuilder,
+            itemHeight: itemHeight,
+            selections: selections,
+            selectionMagnification: selectionMagnification,
+            selectorHeight: selectorHeight,
+            selectorWidth: selectorWidth,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NestedSelectionView<T> extends StatelessWidget {
+  final List<SelectionItemModel<T>> items;
+  final List<T>? selections;
+  final Widget Function(SelectionItemModel<T>)? itemBuilder;
+  final Widget Function(SelectionItemModel<T>)? selectedItemBuilder;
+  final void Function(List<T>) onSelectionChanged;
+  final double selectionMagnification;
+  final double selectorHeight;
+  final double itemHeight;
+  final double selectorWidth;
+
+  const NestedSelectionView(
+      {super.key,
+      required this.items,
+      this.selections,
+      this.itemBuilder,
+      this.selectedItemBuilder,
+      required this.onSelectionChanged,
+      this.selectorHeight = 250,
+      this.selectionMagnification = 1,
+      this.itemHeight = 40,
+      this.selectorWidth = 120});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: getListsAndSelections()
+              .mapIndexed((i, e) => SelectionView<T>(
+                    items: e.$2,
+                    selection: e.$1,
+                    onSelectionChanged: (s) {
+                      List<T> newSelections;
+                      //
+                      if (selections == null || selections!.isEmpty) {
+                        newSelections = [s];
+                      } else if (selections!.length <= i) {
+                        //selections!.add(s);
+                        newSelections = selections!..add(s);
+                      } else {
+                        newSelections = [];
+                        selections![i] = s;
+                        // inner selections should be set null
+                        for (int j = 0; j <= i; j++) {
+                          newSelections.add(selections![j]);
+                        }
+                      }
+                      onSelectionChanged(newSelections);
+                    },
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  Iterable<(T?, List<SelectionItemModel<T>>)> getListsAndSelections() sync* {
+    //int index = 0;
+    //
+    if (selections == null || selections!.isEmpty) {
+      yield (null, items);
+      return;
+    }
+    //
+    List<SelectionItemModel<T>>? currentItems = items;
+    for (var selection in selections!) {
+      yield (selection, currentItems!);
+      //
+      currentItems = currentItems.firstWhere((element) => element.value == selection).children;
+    }
+    //
+    if (currentItems != null) {
+      yield (null, currentItems);
+    }
+  }
+
+  List<T> getDefaultSelections() {
+    var selections = <T>[];
+
+    List<SelectionItemModel<T>>? currentItems = items;
+    //
+    while (currentItems != null) {
+      selections.add(currentItems.first.value);
+      //
+      currentItems = currentItems.first.children;
+    }
+    //
+    return selections;
+  }
+}
+
+class SelectionView<T> extends StatelessWidget {
+  final List<SelectionItemModel<T>> items;
+  final T? selection;
+  final Widget Function(SelectionItemModel<T>)? itemBuilder;
+  final Widget Function(SelectionItemModel<T>)? selectedItemBuilder;
+  final void Function(T) onSelectionChanged;
+  final double selectionMangnification;
+  final double selectorHeight;
+  final double itemHeight;
+  final double selectorWidth;
+
+  const SelectionView(
+      {super.key,
+      required this.items,
+      this.selection,
+      this.itemBuilder,
+      this.selectedItemBuilder,
+      required this.onSelectionChanged,
+      this.selectorHeight = 250,
+      this.selectionMangnification = 1,
+      this.itemHeight = 40,
+      this.selectorWidth = 120});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: selectorHeight,
+        width: selectorWidth,
+        child: ListWheelScrollView(
+          magnification: selectionMangnification,
+          onSelectedItemChanged: (i) => onSelectionChanged(items[i].value),
+          useMagnifier: selectionMangnification > 1,
+          itemExtent: itemHeight,
+          controller: FixedExtentScrollController(initialItem: items.indexOfWhere((e) => e.value == selection) ?? 0),
+          physics: const FixedExtentScrollPhysics(),
+          children: itemBuilder == null
+              ? items
+                  .map(
+                    (e) => Container(
+                      padding: const EdgeInsets.all(5),
+                      alignment: Alignment.center,
+                      child: Text(e.label,
+                          style: e.value == selection ? TextStyle(fontWeight: FontWeight.bold, fontSize: 25) : null),
+                    ),
+                  )
+                  .toList()
+              : items
+                  .map(
+                    (e) => (e.value == selection && selectedItemBuilder != null)
+                        ? selectedItemBuilder!(e)
+                        : itemBuilder!(e),
+                  )
+                  .toList(),
+        ));
+  }
+}
+
+class SelectionItemModel<T> {
+  final String label;
+  final T value;
+  final List<SelectionItemModel<T>>? children;
+
+  SelectionItemModel({required this.label, required this.value, this.children});
+}
+
+extension IterableExtensions<Y> on Iterable<Y> {
+  Iterable<T> mapIndexed<T>(T Function(int index, Y item) mapper) sync* {
+    int i = 0;
+    for (final element in this) {
+      yield mapper(i, element);
+      i++;
+    }
+  }
+
+  int? indexOfWhere(bool Function(Y item) cond) {
+    var index = 0;
+    for (final element in this) {
+      if (cond(element)) {
+        return index;
+      }
+      index++;
+    }
+    return null;
+  }
+}
